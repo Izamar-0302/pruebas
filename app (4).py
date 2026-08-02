192a06a126341db06be5e649484affc1e50bb3b1
Izamar-0302 <angeleseuceda02@gmail.com>
2026-08-02 07:56:14 -0600

    Rename config file path to config.json

--- app (4).py
+++ app (4).py
@@ -27,7 +27,7 @@ st.set_page_config(
 MODEL_DIR = "model"
 MODEL_PATH_KERAS = os.path.join(MODEL_DIR, "agrodetect_mobilenetv2.keras.zip")
 MODEL_PATH_H5 = os.path.join(MODEL_DIR, "model.weights.h5")
-CONFIG_PATH = os.path.join(MODEL_DIR, "config_app.json")
+CONFIG_PATH = os.path.join(MODEL_DIR, "config.json")
 
 CLASES_DEFAULT = ["sana", "roya", "cercospora", "phoma", "arana_roja", "minador"]
 IMG_SIZE_DEFAULT = (224, 224)
